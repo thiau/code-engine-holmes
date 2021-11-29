7b0691fa2feb4f634f78f627aae0dd143d8faa94
Thiago Araujo <thiau.augusto@gmail.com>
2021-11-29 20:33:47 +0000

    Update response

--- server/routes/test.py
+++ server/routes/test.py
@@ -6,5 +6,5 @@ from flask import Flask, jsonify, request
 class TestApi(Resource):
     def get(self):
         return {
-            "response": "it works now! (Updated on Nov 29 - 17:16 BRT)"
+            "response": "it works now! (Updated on Nov 29 - 17:33 BRT)"
         }
